77d5be7b1cfd51b15dba866d1d91fecd9e73879a
Brian McLacken <bdmclacken@gmail.com>
2017-04-29 09:58:25 -0700

    My first commit

--- DIKU/ViewController.swift
+++ DIKU/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        self.theLabel.text = "Hello there!"
+        print("Button tapped")
+        tapCount = tapCount + 1
+        print(tapCount)
+        if tapCount >= 10 {
+            self.theLabel.text = "Wow you're excited with that button!"
+        }
+    }
+    
+    @IBAction func buttonPressedNext(_ sender: Any) {
+        self.theLabel.text = "Buttons are cool!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
